9e490bbe7302fb678d3887ab80529f9ab9c9962c
Errol Elliott <ellioe03@mc-s095509.local>
2017-04-23 14:19:16 +0100

    Update to use intellij

--- alternatingSplit/src/main/java/com/itshackney/app/Kata.java
+++
@@ -1,4 +0,0 @@
-package com.itshackney.app;
-
-public class Kata {
-}
---
+++ alternatingSplit/src/main/java/itshackney/Alternating.java
@@ -0,0 +1,9 @@
+package itshackney;
+
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+class Alternating {
+
+
+}
